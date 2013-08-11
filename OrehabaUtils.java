08f53a2d03af5e0f189d113a16d3e5cc5fe26ef4
NurseAngel <yuubiseiharukana@gmail.com>
2013-08-11 21:28:33 +0900

    ・範囲外のブロックが壊せなくなりました ・完全自由スペース対応

--- OrehabaUtils.java
+++ OrehabaUtils.java
@@ -51,7 +51,7 @@ public class OrehabaUtils {
 
 		/**
 		 * TODO 今立っているブロックを取得したい。<br />
-		 * 上記xzは現在居る場所なので、スニークではみ出るとそのブロックになる。
+		 * 上記xzは現在居る場所なので、スニークではみ出るとはみ出た先のブロックになる。
 		 *
 		 * 現在はとりあえず<br />
 		 * 足下9ブロックの真下岩盤までのうちいずれかにダイヤブロックがあればいい、とする。
@@ -66,11 +66,57 @@ public class OrehabaUtils {
 					if (blockID == Block.blockDiamond.blockID) {
 						return true;
 					}
+					// 黒曜石であれば、周囲3ブロック内にダイヤがあるか探す
+					if ((blockID == Block.obsidian.blockID) && (x == i) && (z == k)) {
+						if (isFoundDiamond(world, x, j, z, blockX, blockY, blockZ, direction)) {
+							return true;
+						}
+					}
+
 				}
 			}
 		}
 
 		// 範囲外
+		return false;
+	}
+
+	/**
+	 * 同じ高さの5*5ブロック内にダイヤブロックがあるかを探す<br />
+	 * 完全自由スペース用
+	 *
+	 * @param playerX
+	 * @param playerY
+	 * @param playerZ
+	 * @param blockX
+	 *            十字架の位置と方向。使わない
+	 * @param blockY
+	 * @param blockZ
+	 * @param direction
+	 * @return
+	 */
+	public static boolean isFoundDiamond(World world, double playerX, double playerY, double playerZ, int blockX, int blockY, int blockZ, int direction) {
+
+		int x = MathHelper.floor_double(playerX);
+		int y = MathHelper.floor_double(playerY);
+		int z = MathHelper.floor_double(playerZ);
+
+		/**
+		 * 完全自由スペースの場合、真下と周囲3*3にダイヤがない可能性がある。<br />
+		 * そのため周囲5*5まで探索する。<br />
+		 * これだと実はダイヤ-黒曜石*4-ダイヤの道で外に行けてしまうのだが、まあそれくらいはいいでしょう。
+		 */
+
+		int blockID;
+		for (int i = x - 2; i <= x + 2; i++) {
+			for (int k = z - 2; k <= z + 2; k++) {
+				blockID = world.getBlockId(i, y, k);
+				if (blockID == Block.blockDiamond.blockID) {
+					return true;
+				}
+			}
+		}
+
 		return false;
 	}
 
--- handler/OrehabaPlayerEtcHandler.java
+++ handler/OrehabaPlayerEtcHandler.java
@@ -1,11 +1,14 @@
 package mods.nurseangel.orehaba.handler;
 
 import mods.nurseangel.orehaba.OrehabaUtils;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.player.FillBucketEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 /**
  * ブロックを破壊したときに呼ばれるイベントがほしいのだが今のところ無いようだ。<br />
@@ -46,13 +49,66 @@ public class OrehabaPlayerEtcHandler {
 	}
 
 	/**
-	 * TODO 実装予定<br />
-	 * 現在ハンドラがないので実装できない
+	 * 範囲外でも壊せるブロック
+	 */
+	public static int[] allowedBlock = { Block.leaves.blockID, Block.web.blockID, Block.web.blockID, Block.vine.blockID, Block.tallGrass.blockID,
+			Block.deadBush.blockID, Block.plantYellow.blockID, Block.plantRed.blockID, Block.mushroomBrown.blockID, Block.mushroomRed.blockID,
+			Block.torchWood.blockID, Block.fire.blockID, Block.crops.blockID, Block.signPost.blockID, Block.lever.blockID, Block.stoneButton.blockID,
+			Block.torchRedstoneIdle.blockID, Block.torchRedstoneActive.blockID, Block.cake.blockID, Block.trapdoor.blockID, Block.carrot.blockID,
+			Block.potato.blockID, Block.woodenButton.blockID };
+
+	/**
+	 * ブロックをクリックしたイベント
 	 *
-	 * ・ブロックを破壊したイベント<br />
-	 * 範囲外であれば、葉などの破壊許可ブロック以外は破壊不可
+	 * @param event
 	 *
-	 * ・スティッキーピストンのイベント<br />
+	 */
+	@ForgeSubscribe
+	public void etcPlayerInteractEvent(PlayerInteractEvent event) {
+
+		// 現在ハンドラが有効でなければそのまま
+		if (!OrehabaTickHandler.isInEffective()) {
+			return;
+		}
+
+		// RIGHT_CLICK_AIR, RIGHT_CLICK_BLOCK, LEFT_CLICK_BLOCKの3種類。<br />
+		// 左クリック以外は不要
+		if (event.action != PlayerInteractEvent.Action.LEFT_CLICK_BLOCK) {
+			return;
+		}
+
+		World world = event.entityPlayer.worldObj;
+
+		/**
+		 * クリック先が範囲内であれば掘ってもよいとする<br />
+		 * isInAreaDirectionではなくisInAreaにしないと、自由スペースが整地できない。
+		 */
+		if (OrehabaUtils.isInArea(world, event.x + 0.5, event.y, event.z + 0.5, OrehabaTickHandler.blockX, 0, OrehabaTickHandler.blockZ,
+				OrehabaTickHandler.direction)) {
+			return;
+		}
+		// 範囲外でも特定のブロックであればなにもしない
+		int blockId = world.getBlockId(event.x, event.y, event.z);
+		for (int i = 0; i < allowedBlock.length; i++) {
+			if (allowedBlock[i] == blockId) {
+				return;
+			}
+		}
+
+		// キャンセル
+		if (event.isCancelable()) {
+			event.setCanceled(true);
+		}
+		/**
+		 * TODO このキャンセル、"壊したけど元に戻る"という見た目になる。<br />
+		 * 岩盤みたいに単に壊せなくする方法があればそうしたい。
+		 */
+	}
+
+	/**
+	 * TODO 実装したい<br />
+	 *
+	 * ・ピストンのイベント<br />
 	 * 範囲外からは引き寄せられなくする
 	 *
 	 */
